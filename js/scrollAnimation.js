4c105222d7b5b92b0ec1fbedba35045d05eb419a
Abraham Arreola <abrahamarrolab198@gmail.com>
2020-09-24 13:38:01 -0500

    Contact me section and scroll animation added

---
+++ js/scrollAnimation.js
@@ -0,0 +1,49 @@
+(function($){
+    'use strict';
+
+    /*-----------------------------------------------------------------
+    Scroll animation
+    -----------------------------------------------------------------*/
+    window.sr = new scrollReveal();
+    
+    $(document).ready(function () {
+	    $(document).on("scroll", onScroll);
+	    
+		//smoothscroll
+	    $('a[href^="#"]').on('click', function (e) {
+	        e.preventDefault();
+	        $(document).off("scroll");
+	        
+	        $('a').each(function () {
+	            $(this).removeClass('active');
+	        })
+	        $(this).addClass('active');
+	      
+	        var target = this.hash,
+	        menu = target;
+	       	var target = $(this.hash);
+	        $('html, body').stop().animate({
+	            scrollTop: (target.offset().top) - 70
+	        }, 500, 'swing', function () {
+	            window.location.hash = target;
+	            $(document).on("scroll", onScroll);
+	        });
+	    });
+	});
+	
+	function onScroll(event){
+	    var scrollPos = $(document).scrollTop();
+	    $('.nav a').each(function () {
+	        var currLink = $(this);
+	        var refElement = $(currLink.attr("href"));
+	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+	            $('.nav ul li a').removeClass("active");
+	            currLink.addClass("active");
+	        }
+	        else{
+	            currLink.removeClass("active");
+	        }
+	    });
+	}
+    /*-----------------------------------------------------------------*/
+})(jQuery)
