1c04e9f3f537d6f17c7e23469250c6957aaef3eb
Abraham Arreola <abrahamarrolab198@gmail.com>
2020-09-24 21:07:08 -0500

    Bug in education panel section fixed

--- js/scrollAnimation.js
+++ js/scrollAnimation.js
@@ -1,49 +1,28 @@
-(function($){
-    'use strict';
+(function ($) {
+	'use strict';
 
-    /*-----------------------------------------------------------------
-    Scroll animation
-    -----------------------------------------------------------------*/
-    window.sr = new scrollReveal();
-    
-    $(document).ready(function () {
-	    $(document).on("scroll", onScroll);
-	    
+	/*-----------------------------------------------------------------
+	Scroll animation
+	-----------------------------------------------------------------*/
+	window.sr = new scrollReveal();
+
+	$(document).ready(function () {
 		//smoothscroll
-	    $('a[href^="#"]').on('click', function (e) {
-	        e.preventDefault();
-	        $(document).off("scroll");
-	        
-	        $('a').each(function () {
-	            $(this).removeClass('active');
-	        })
-	        $(this).addClass('active');
-	      
-	        var target = this.hash,
-	        menu = target;
-	       	var target = $(this.hash);
-	        $('html, body').stop().animate({
-	            scrollTop: (target.offset().top) - 70
-	        }, 500, 'swing', function () {
-	            window.location.hash = target;
-	            $(document).on("scroll", onScroll);
-	        });
-	    });
+		$('a[href^="#"]').on('click', function (e) {
+			if ($(this).attr("id") !== "education-tab" && $(this).attr("id") !== "courses-tab") {
+				e.preventDefault();
+				$(document).off("scroll");
+
+				var target = this.hash,
+					menu = target;
+				var target = $(this.hash);
+				$('html, body').stop().animate({
+					scrollTop: (target.offset().top) - 70
+				}, 500, 'swing', function () {
+					window.location.hash = target;
+				});
+			}
+		});
 	});
-	
-	function onScroll(event){
-	    var scrollPos = $(document).scrollTop();
-	    $('.nav a').each(function () {
-	        var currLink = $(this);
-	        var refElement = $(currLink.attr("href"));
-	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-	            $('.nav ul li a').removeClass("active");
-	            currLink.addClass("active");
-	        }
-	        else{
-	            currLink.removeClass("active");
-	        }
-	    });
-	}
-    /*-----------------------------------------------------------------*/
+	/*-----------------------------------------------------------------*/
 })(jQuery)
